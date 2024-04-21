8f38b6c6c466e0f250fff05da367458fe98e2614
jmth266 <130870809+jmth266@users.noreply.github.com>
2024-04-21 18:12:50 -0400

    Update Navbar.jsx
    adds back button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,49 @@
-import {
-  Link 
-} from "react-router-dom";
-import SearchBar from './searchBar'; // Import the SearchBar component
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import SearchBar from './searchBar';
+import LeftArrowIcon from './left_arrow.png'; // Import SVG file
 
-// Define a functional component for the navigation bar
+
 function NavBar() {
+  const navigate = useNavigate(); // Use useNavigate hook to get navigation function
+
+  const goBack = () => {
+    navigate(-1); // Navigate back one step in history
+  };
+  const disabledPaths = ['/Calculator', '/HomePage','/SearchPage'];
+
+  // Check if the current location matches any of the disabled paths
+  const disableBackButton = disabledPaths.includes(location.pathname);
   return (
-    <nav className="navbar  navbar-expand navbar-light bg-light ">
-      <div className="container-fluid ">
-        <ul className="nav navbar-nav ">
-          
-          <li className="nav-item">
-            <Link to ="/Calculator" className="nav-link">Calculator</Link>
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="container-fluid">
+        <ul className="nav navbar-nav">
+        {/* Conditionally render the back button based on the disableBackButton condition */}
+
+        {!disableBackButton && (
+            <li className="nav-item">
+              <button className="btn btn-link nav-link" onClick={goBack}>
+                <img src={LeftArrowIcon} alt="Left arrow icon" style={{ width: '24px', height: '24px' }} /> {/* Render SVG icon */}
+              </button>
+            </li>
+          )}
+        <li className="nav-item">
+            <Link to="/HomePage" className="nav-link">Home</Link>
           </li>
           <li className="nav-item">
-            <Link to ="SearchPage" className="nav-link">Search</Link>
+            <Link to="/Calculator" className="nav-link">Calculator</Link>
           </li>
-        
           <li className="nav-item">
-        <SearchBar /> {/* Render the SearchBar component within the navigation bar */}
-        </li>
+            <Link to="/SearchPage" className="nav-link">Search</Link>
+          </li>
+          <li className="nav-item">
+            <SearchBar />
+          </li>
+  
         </ul>
-        
       </div>
     </nav>
   );
 }
 
-export default NavBar; // Export the NavBar component
+export default NavBar;
